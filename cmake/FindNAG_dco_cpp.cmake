0bc8dd46aad5b597fa2a464c85d56fd2f11845fb
Alexander Fleming <fleming@stce.rwth-aachen.de>
2024-04-04 17:46:01 +0200

    remove win32 from find dco

--- cmake/FindNAG_dco_cpp.cmake
+++ cmake/FindNAG_dco_cpp.cmake
@@ -7,46 +7,22 @@ endif()
 
 # Tries to retrieve installation path from given product code
 function(_dco_get_installation_path prodcode installation_path)
-  if(WIN32)
-    set(_dco_appid "\\{8B8241FE-4770-49CB-B334-1E6${prodcode}\\}_is1")
-    string(CONCAT _dco_reg_path "HKLM\\SOFTWARE\\WOW6432Node\\Microsoft\\"
-      "Windows\\CurrentVersion\\Uninstall")
-    execute_process(
-      COMMAND "reg" "query" "${_dco_reg_path}\\${_dco_appid}"
-      OUTPUT_VARIABLE _installation_path_loc
-      ERROR_QUIET)
-
-    if(_installation_path_loc)
-      string(REGEX MATCH "InstallLocation[ ]*REG_SZ[ ]*([^\n]*)"
-        _installation_path_loc ${_installation_path_loc})
-      string(REGEX REPLACE "InstallLocation[ ]*REG_SZ[ ]*" ""
-        _installation_path_loc ${_installation_path_loc})
-    endif()
-
-    set(${installation_path}
-      ${_installation_path_loc}
-      PARENT_SCOPE)
-  else()
-    # Search default installation directories
-    set(_dco_def_dirs $ENV{HOME}/NAG/${prodcode} /opt/NAG/${prodcode} /opt/${prodcode})
-    find_file(
-      _installation_path_loc dco.hpp
-      PATHS ${_dco_def_dirs}
-      PATH_SUFFIXES include
-      NO_DEFAULT_PATH)
-
-    if(_installation_path_loc)
-      get_filename_component(_installation_path_loc ${_installation_path_loc}
-        DIRECTORY)
-      string(REPLACE "/include" "" _installation_path_loc
-        ${_installation_path_loc})
-    endif()
-
-    set(${installation_path}
-      ${_installation_path_loc}
-      PARENT_SCOPE)
+  # Search default installation directories
+  set(_dco_def_dirs $ENV{HOME}/NAG/${prodcode} /opt/NAG/${prodcode} /opt/${prodcode})
+  find_file(
+    _installation_path_loc dco.hpp
+    PATHS ${_dco_def_dirs}
+    PATH_SUFFIXES include
+    NO_DEFAULT_PATH)
+  if(_installation_path_loc)
+    get_filename_component(_installation_path_loc ${_installation_path_loc}
+      DIRECTORY)
+    string(REPLACE "/include" "" _installation_path_loc
+      ${_installation_path_loc})
   endif()
-
+  set(${installation_path}
+    ${_installation_path_loc}
+    PARENT_SCOPE)
   unset(_installation_path_loc CACHE)
 endfunction()
 
@@ -84,177 +60,43 @@ if(NOT(NAG_dco_cpp_DIR))
     set(NAG_dco_cpp_DIR ${NAG_dco_cpp_INCLUDE_DIR}/..)
     get_filename_component(NAG_dco_cpp_DIR "${NAG_dco_cpp_DIR}" ABSOLUTE)
   else()
-    if(WIN32)
-      if(CMAKE_C_COMPILER_VERSION MATCHES "19.(.*)")
-        set(VS_NEWER_THAN_15 1)
-      else()
-        set(VS_NEWER_THAN_15 0)
+    # LINUX
+    foreach(_dco_version_major 9;8;7;6;5;4;3;2)
+      if(NAG_dco_cpp_DIR)
+        break()
       endif()
 
-      foreach(_dco_version_major 9;8;2;7;6;5;4;3;2)
+      foreach(_dco_version_minor 9;8;7;6;5;4;3;2;1;0)
+        # Try to find non-license-managed version
+        _dco_get_installation_path("dclin${_dco_version_major}${_dco_version_minor}nn" NAG_dco_cpp_DIR)
         if(NAG_dco_cpp_DIR)
           break()
         endif()
 
-        foreach(
-          _dco_version_minor 9;8;7;6;5;4;3;2;1;0)
-          # Try to find non-license-managed version
-          _dco_get_installation_path("dcwin${_dco_version_major}${_dco_version_minor}nn" NAG_dco_cpp_DIR)
-
-          if(NAG_dco_cpp_DIR)
-            break()
-          endif()
-
-          # Try to find license-managed version 64-bit pre 2015
-          _dco_get_installation_path("dcw6i${_dco_version_major}${_dco_version_minor}na" NAG_dco_cpp_DIR)
-
-          if(NAG_dco_cpp_DIR AND VS_NEWER_THAN_15)
-            if(NOT(NAG_dco_cpp_FIND_QUIETLY))
-              message(
-                STATUS
-                "NAG config - dco/c++: VS pre 2015 version found here: "
-                "'${NAG_dco_cpp_DIR}'. This is not matching your currently "
-                "loaded environment. Continue searching.")
-            endif()
-          endif()
-
-          if(NAG_dco_cpp_DIR AND _dco_is_32)
-            if(NOT(NAG_dco_cpp_FIND_QUIETLY))
-              message(
-                STATUS
-                "NAG config - dco/c++: 64-bit version found here: "
-                "'${NAG_dco_cpp_DIR}'. This is not matching your currently "
-                "loaded environment. Continue searching.")
-            endif()
-          endif()
-
-          if(NAG_dco_cpp_DIR AND NOT(VS_NEWER_THAN_15) AND _dco_is_64)
-            break()
-          endif()
-
-          # Try to find license-managed version 64-bit post 2015
-          _dco_get_installation_path("dcw6i${_dco_version_major}${_dco_version_minor}ne" NAG_dco_cpp_DIR)
-
-          if(NAG_dco_cpp_DIR AND NOT(VS_NEWER_THAN_15))
-            if(NOT(NAG_dco_cpp_FIND_QUIETLY))
-              message(
-                STATUS
-                "NAG config - dco/c++: VS post 2015 version found here: "
-                "'${NAG_dco_cpp_DIR}'. This is not matching your currently "
-                "loaded environment. Continue searching.")
-            endif()
-          endif()
-
-          if(NAG_dco_cpp_DIR AND _dco_is_32)
-            if(NOT(NAG_dco_cpp_FIND_QUIETLY))
-              message(
-                STATUS
-                "NAG config - dco/c++: 64-bit version found here: "
-                "'${NAG_dco_cpp_DIR}'. This is not matching your currently "
-                "loaded environment. Continue searching.")
-            endif()
-          endif()
-
-          if(NAG_dco_cpp_DIR AND VS_NEWER_THAN_15 AND _dco_is_64)
-            break()
-          endif()
-
-          # Try to find license-managed version 32-bit pre 2015
-          _dco_get_installation_path("dcw32${_dco_version_major}${_dco_version_minor}na" NAG_dco_cpp_DIR)
-
-          if(NAG_dco_cpp_DIR AND VS_NEWER_THAN_15)
-            if(NOT(NAG_dco_cpp_FIND_QUIETLY))
-              message(
-                STATUS
-                "NAG config - dco/c++: VS pre 2015 version found here: "
-                "'${NAG_dco_cpp_DIR}'. This is not matching your currently "
-                "loaded environment. Continue searching.")
-            endif()
-          endif()
-
-          if(NAG_dco_cpp_DIR AND _dco_is_64)
-            if(NOT(NAG_dco_cpp_FIND_QUIETLY))
-              message(
-                STATUS
-                "NAG config - dco/c++: 32-bit version found here: "
-                "'${NAG_dco_cpp_DIR}'. This is not matching your currently "
-                "loaded environment. Continue searching.")
-            endif()
-          endif()
-
-          if(NAG_dco_cpp_DIR AND NOT(VS_NEWER_THAN_15) AND _dco_is_64)
-            break()
-          endif()
-
-          # Try to find license-managed version 32-bit post 2015
-          _dco_get_installation_path("dcw32${_dco_version_major}${_dco_version_minor}ne" NAG_dco_cpp_DIR)
-
-          if(NAG_dco_cpp_DIR AND NOT(VS_NEWER_THAN_15))
-            if(NOT(NAG_dco_cpp_FIND_QUIETLY))
-              message(
-                STATUS
-                "NAG config - dco/c++: VS post 2015 version found here: "
-                "'${NAG_dco_cpp_DIR}'. This is not matching your currently "
-                "loaded environment. Continue searching.")
-            endif()
-          endif()
-
-          if(NAG_dco_cpp_DIR AND _dco_is_64)
-            if(NOT(NAG_dco_cpp_FIND_QUIETLY))
-              message(
-                STATUS
-                "NAG config - dco/c++: 32-bit version found here: "
-                "'${NAG_dco_cpp_DIR}'. This is not matching your currently "
-                "loaded environment. Continue searching.")
-            endif()
-          endif()
-
-          if(NAG_dco_cpp_DIR AND VS_NEWER_THAN_15 AND _dco_is_32)
-            break()
-          endif()
-        endforeach()
-      endforeach()
-    else() # LINUX
-      foreach(_dco_version_major 9;8;7;6;5;4;3;2)
-        if(NAG_dco_cpp_DIR)
+        # Try to find license-managed version 64-bit
+        _dco_get_installation_path("dcl6i${_dco_version_major}${_dco_version_minor}ngl" NAG_dco_cpp_DIR)
+        if(NAG_dco_cpp_DIR AND _dco_is_64)
           break()
         endif()
 
-        foreach(_dco_version_minor 9;8;7;6;5;4;3;2;1;0)
-          # Try to find non-license-managed version
-          _dco_get_installation_path("dclin${_dco_version_major}${_dco_version_minor}nn" NAG_dco_cpp_DIR)
-
-          if(NAG_dco_cpp_DIR)
-            break()
+        # Try to find license-managed version 32-bit
+        _dco_get_installation_path("dclux${_dco_version_major}${_dco_version_minor}ngl" NAG_dco_cpp_DIR)
+        if(NAG_dco_cpp_DIR AND _dco_is_64)
+          if(NOT(NAG_dco_cpp_FIND_QUIETLY))
+            message(
+              STATUS
+              "NAG config - dco/c++: 32-bit version found here: "
+              "'${NAG_dco_cpp_DIR}'. Continue searching. If you really want "
+              "to use this 32-bit version on your 64-bit system, please "
+              "specify NAG_dco_cpp_DIR explicitly.")
           endif()
+        endif()
 
-          # Try to find license-managed version 64-bit
-          _dco_get_installation_path("dcl6i${_dco_version_major}${_dco_version_minor}ngl" NAG_dco_cpp_DIR)
-
-          if(NAG_dco_cpp_DIR AND _dco_is_64)
-            break()
-          endif()
-
-          # Try to find license-managed version 32-bit
-          _dco_get_installation_path("dclux${_dco_version_major}${_dco_version_minor}ngl" NAG_dco_cpp_DIR)
-
-          if(NAG_dco_cpp_DIR AND _dco_is_64)
-            if(NOT(NAG_dco_cpp_FIND_QUIETLY))
-              message(
-                STATUS
-                "NAG config - dco/c++: 32-bit version found here: "
-                "'${NAG_dco_cpp_DIR}'. Continue searching. If you really want "
-                "to use this 32-bit version on your 64-bit system, please "
-                "specify NAG_dco_cpp_DIR explicitly.")
-            endif()
-          endif()
-
-          if(NAG_dco_cpp_DIR AND _dco_is_32)
-            break()
-          endif()
-        endforeach()
+        if(NAG_dco_cpp_DIR AND _dco_is_32)
+          break()
+        endif()
       endforeach()
-    endif()
+    endforeach()
   endif()
 else()
   if(NOT(IS_ABSOLUTE ${NAG_dco_cpp_DIR}))
@@ -323,31 +165,10 @@ unset(_DCO_VERSION_HPP CACHE)
 
 # Licensed version? => search library
 # ##############################################################################
-if(WIN32)
-  set(_dco_library_name libdcoc)
-  find_library(
-    NAG_dco_cpp_LIBRARY_MD
-    NAMES ${_dco_library_name}_MD
-    HINTS ${NAG_dco_cpp_DIR}/lib)
-  find_library(
-    NAG_dco_cpp_LIBRARY_MT
-    NAMES ${_dco_library_name}_MT
-    HINTS ${NAG_dco_cpp_DIR}/lib)
-  find_library(
-    NAG_dco_cpp_LIBRARY_MDd
-    NAMES ${_dco_library_name}_MDd
-    HINTS ${NAG_dco_cpp_DIR}/lib)
-  find_library(
-    NAG_dco_cpp_LIBRARY_MTd
-    NAMES ${_dco_library_name}_MTd
-    HINTS ${NAG_dco_cpp_DIR}/lib)
-  set(NAG_dco_cpp_LIBRARY ${NAG_dco_cpp_LIBRARY_MD})
-else()
-  find_library(
-    NAG_dco_cpp_LIBRARY
-    NAMES dcoc
-    HINTS ${NAG_dco_cpp_DIR}/lib)
-endif()
+find_library(
+  NAG_dco_cpp_LIBRARY
+  NAMES dcoc
+  HINTS ${NAG_dco_cpp_DIR}/lib)
 
 get_filename_component(_dco_library_dir ${NAG_dco_cpp_LIBRARY} DIRECTORY)
 
@@ -414,64 +235,9 @@ else()
   set(NAG_dco_cpp_FOUND 1)
 
   if(NAG_dco_cpp_LIBRARY)
-    if(WIN32)
-      set(_dco_cpp_compile_options
-        "/NODEFAULTLIB:MSVCRT$<$<NOT:$<CONFIG:Debug>>:D>"
-        "/NODEFAULTLIB:LIBCMT$<$<NOT:$<CONFIG:Debug>>:D>")
-
-      add_library(NAG::dco_cpp_MT STATIC IMPORTED)
-      set_target_properties(
-        NAG::dco_cpp_MT PROPERTIES IMPORTED_LOCATION
-        "${NAG_dco_cpp_LIBRARY_MT}")
-      set_target_properties(
-        NAG::dco_cpp_MT PROPERTIES INTERFACE_LINK_OPTIONS
-        "${_dco_cpp_compile_options}")
-      add_library(NAG::dco_cpp_MTd STATIC IMPORTED)
-      set_target_properties(
-        NAG::dco_cpp_MTd PROPERTIES IMPORTED_LOCATION
-        "${NAG_dco_cpp_LIBRARY_MTd}")
-      set_target_properties(
-        NAG::dco_cpp_MTd PROPERTIES INTERFACE_LINK_OPTIONS
-        "${_dco_cpp_compile_options}")
-      add_library(NAG::dco_cpp_MD STATIC IMPORTED)
-      set_target_properties(
-        NAG::dco_cpp_MD PROPERTIES IMPORTED_LOCATION
-        "${NAG_dco_cpp_LIBRARY_MD}")
-      set_target_properties(
-        NAG::dco_cpp_MD PROPERTIES INTERFACE_LINK_OPTIONS
-        "${_dco_cpp_compile_options}")
-      add_library(NAG::dco_cpp_MDd STATIC IMPORTED)
-      set_target_properties(
-        NAG::dco_cpp_MDd PROPERTIES IMPORTED_LOCATION
-        "${NAG_dco_cpp_LIBRARY_MDd}")
-      set_target_properties(
-        NAG::dco_cpp_MDd PROPERTIES INTERFACE_LINK_OPTIONS
-        "${_dco_cpp_compile_options}")
-
-      add_library(NAG::dco_cpp INTERFACE IMPORTED GLOBAL)
-
-      if((CMAKE_MSVC_RUNTIME_LIBRARY MATCHES "DLL") OR(NAG_USE_DYNAMIC_RUNTIME
-        ))
-        set_target_properties(
-          NAG::dco_cpp PROPERTIES INTERFACE_LINK_LIBRARIES
-          "NAG::dco_cpp_MD$<$<CONFIG:Debug>:d>")
-      else()
-        set_target_properties(
-          NAG::dco_cpp PROPERTIES INTERFACE_LINK_LIBRARIES
-          "NAG::dco_cpp_MT$<$<CONFIG:Debug>:d>")
-      endif()
-
-      set_property(
-        TARGET NAG::dco_cpp
-        APPEND
-        PROPERTY INTERFACE_LINK_LIBRARIES "user32" "advapi32" "ws2_32")
-      set_target_properties(NAG::dco_cpp PROPERTIES INTERFACE_LINK_DIRECTORIES
-        "${_dco_library_dir}")
-    else()
-      add_library(NAG::dco_cpp STATIC IMPORTED GLOBAL)
-      set_target_properties(NAG::dco_cpp PROPERTIES IMPORTED_LOCATION
-        "${NAG_dco_cpp_LIBRARY}")
-    endif()
+    add_library(NAG::dco_cpp STATIC IMPORTED GLOBAL)
+    set_target_properties(NAG::dco_cpp PROPERTIES IMPORTED_LOCATION
+      "${NAG_dco_cpp_LIBRARY}")
   else()
     add_library(NAG::dco_cpp INTERFACE IMPORTED GLOBAL)
   endif()
@@ -483,9 +249,3 @@ endif()
 unset(NAG_dco_cpp_INCLUDE_DIR CACHE)
 unset(NAG_dco_cpp_LIBRARY CACHE)
 
-if(WIN32)
-  unset(NAG_dco_cpp_LIBRARY_MD CACHE)
-  unset(NAG_dco_cpp_LIBRARY_MT CACHE)
-  unset(NAG_dco_cpp_LIBRARY_MDd CACHE)
-  unset(NAG_dco_cpp_LIBRARY_MTd CACHE)
-endif()
