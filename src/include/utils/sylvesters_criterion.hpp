f6554cc3205a1d5d30588e4cb24d95a833be611e
Alexander Fleming <fleming@stce.rwth-aachen.de>
2024-04-17 16:07:33 +0200

    bad determinant. oh so bad.

--- src/include/utils/sylvesters_criterion.hpp
+++ src/include/utils/sylvesters_criterion.hpp
@@ -5,6 +5,8 @@
 #include "boost/numeric/interval.hpp"
 
 #include "eigen_interval_extensions.hpp"
+#include <Eigen3.4/src/Core/util/Constants.h>
+#include <type_traits>
 
 /**
  * @brief Check if argument is less than or equal to zero.
@@ -43,14 +45,43 @@ struct drop_idx {
 };
 
 /**
- * @brief Bad determinant calculation.
+ * @brief Inefficient determinant calculation.
  */
-template <typename Derived>
-typename Eigen::MatrixBase<Derived>::Scalar
-bad_determinant(Eigen::MatrixBase<Derived> const &A) {
-  using Eigen::last;
+template <typename T, int N, std::enable_if_t<(N != Eigen::Dynamic), bool> = true>
+typename Eigen::Matrix<T, N, N>::Scalar
+bad_determinant(Eigen::Matrix<T, N, N> const &A) {
   using Eigen::seq;
-  typename Eigen::MatrixBase<Derived>::Scalar det{0};
+  typename Eigen::Matrix<T, N, N>::Scalar det{0};
+  if constexpr (N == 1) {
+    det = A(0, 0);
+  } else if constexpr (N == 2) {
+    det = A(0, 0) * A(1, 1) - A(0, 1) * A(1, 0);
+  } else if constexpr (N == 3) {
+    det += A(0, 0) * (A(1, 1) * A(2, 2) - A(1, 2) * A(2, 1));
+    det -= A(0, 1) * (A(1, 0) * A(2, 2) - A(1, 2) * A(2, 0));
+    det += A(0, 2) * (A(1, 0) * A(2, 1) - A(1, 1) * A(2, 0));
+  } else {
+    // det A = \sum_{i=1}^N -1^(i-1) * A(i,1)* det (i,1 submatrix of A)
+    // even-numbered minors
+    for (int i = 0; i < A.cols(); i += 2) {
+      Eigen::Matrix<T, N - 1, N - 1> minor;
+      minor = A(drop_idx{0, A.rows()}, drop_idx{i, A.cols()});
+      det += A(0, i) * bad_determinant(minor);
+    }
+    // odd-numbered minors
+    for (int i = 1; i < A.cols(); i += 2) {
+      Eigen::Matrix<T, N - 1, N - 1> minor;
+      minor = A(drop_idx{0, A.rows()}, drop_idx{i, A.cols()});
+      det -= A(0, i) * bad_determinant(minor);
+    }
+  }
+  return det;
+}
+
+template <typename T>
+typename Eigen::MatrixX<T>::Scalar bad_determinant(Eigen::MatrixX<T> const &A) {
+  using Eigen::seq;
+  typename Eigen::MatrixX<T>::Scalar det{0};
 
   if (A.rows() == 1) {
     det = A(0, 0);
@@ -60,24 +91,22 @@ bad_determinant(Eigen::MatrixBase<Derived> const &A) {
     det += A(0, 0) * (A(1, 1) * A(2, 2) - A(1, 2) * A(2, 1));
     det -= A(0, 1) * (A(1, 0) * A(2, 2) - A(1, 2) * A(2, 0));
     det += A(0, 2) * (A(1, 0) * A(2, 1) - A(1, 1) * A(2, 0));
+  } else {
+    // det A = \sum_{i=1}^N -1^(i-1) * A(i,1)* det (i,1 submatrix of A)
+    // even-numbered minors
+    for (int i = 0; i < A.cols(); i += 2) {
+      Eigen::MatrixX<T> minor;
+      minor = A(drop_idx{0, A.rows()}, drop_idx{i, A.cols()});
+      det += A(0, i) * bad_determinant(minor);
+      // det += A(drop_idx{0, A.rows()}, drop_idx{i, A.cols()}).rows();
+    }
+    // odd-numbered minors
+    for (int i = 1; i < A.cols(); i += 2) {
+      Eigen::MatrixX<T> minor;
+      minor = A(drop_idx{0, A.rows()}, drop_idx{i, A.cols()});
+      det -= A(0, i) * bad_determinant(minor);
+    }
   }
-  // else
-  // {
-  //     // det A = \sum_{i=1}^N -1^(i-1) * A(i,1)* det (i,1 submatrix of A)
-  //     // even-numbered minors
-  //     for (int i = 0; i < A.cols(); i += 2)
-  //     {
-  //         det += A(0, i) * bad_determinant(A(drop_idx{0, A.rows()},
-  //         drop_idx{i, A.cols()}));
-  //         //det += A(drop_idx{0, A.rows()}, drop_idx{i, A.cols()}).rows();
-  //     }
-  //     // odd-numbered minors
-  //     for (int i = 1; i < A.cols(); i += 2)
-  //     {
-  //         det -= A(0, i) * bad_determinant(A(drop_idx{0, A.rows()},
-  //         drop_idx{i, A.cols()}));
-  //     }
-  // }
   return det;
 }
 
